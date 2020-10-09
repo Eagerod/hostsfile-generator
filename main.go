8cded3b9903aa4b80aa5e2f33b6c1d5d0767ab32
Aleem Haji <hajial@gmail.com>
2020-10-08 21:06:32 -0400

    Moves the hostsfile mechanisms to a pkg.

--- main.go
+++ main.go
@@ -10,40 +10,16 @@ import (
 	"os"
 	"os/signal"
 	"path/filepath"
-	"strings"
-	"sync"
 	"syscall"
 
 	"k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	extensionsv1beta1 "k8s.io/api/extensions/v1beta1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    
+    "github.com/Eagerod/hosts-file-daemon/pkg/hostsfile"
 )
-
-type HostsEntry struct {
-	IP    string
-	Hosts []string
-}
-
-func (he HostsEntry) String() string {
-	return fmt.Sprintf(strings.Join(append([]string{he.IP}, he.Hosts...), "\t"))
-}
-
-type ConcurrentHostsFile struct {
-	Lock    sync.RWMutex
-	Entries map[string]HostsEntry
-}
-
-func (chf *ConcurrentHostsFile) AddHostname(ip string, hostname string) {
-    entry, present := (*chf).Entries[ip]
-    if !present {
-        entry = HostsEntry{ip, []string{}}
-        (*chf).Entries[ip] = entry
-    }
-
-    entry.Hosts = append(entry.Hosts, hostname)
-}
 
 func main() {
 	ip := flag.String("ingress-ip", "", "IP address of the NGINX Ingress Controller.")
@@ -77,15 +53,15 @@ func main() {
 		done <- true
 	}()
 
-	hostsFile := ConcurrentHostsFile{sync.RWMutex{}, map[string]HostsEntry{}}
+	hostsFile := hostsfile.NewConcurrentHostsFile()
 
-	go ManageIngressChanges(clientset, &hostsFile)
-	go ManageServiceChanges(clientset, *searchDomain, &hostsFile)
+	go ManageIngressChanges(clientset, *ip, hostsFile)
+	go ManageServiceChanges(clientset, *searchDomain, hostsFile)
 
 	<-done
 }
 
-func ManageIngressChanges(clientset *kubernetes.Clientset, hosts *ConcurrentHostsFile) {
+func ManageIngressChanges(clientset *kubernetes.Clientset, ingressIP string, hosts *hostsfile.ConcurrentHostsFile) {
 	api := clientset.ExtensionsV1beta1()
 	listOptions := metav1.ListOptions{}
 	watcher, err := api.Ingresses("").Watch(context.Background(), listOptions)
@@ -119,16 +95,12 @@ func ManageIngressChanges(clientset *kubernetes.Clientset, hosts *ConcurrentHost
 			continue
 		}
 
-		hosts.Lock.Lock()
-		hosts.Entries[ingress.Spec.Rules[0].Host] = HostsEntry{"192.168.200.128", []string{ingress.Spec.Rules[0].Host}}
-
-		PrintHostsFile(hosts.Entries)
-		fmt.Println("")
-		hosts.Lock.Unlock()
+        hosts.AddHostname(ingressIP, ingress.Spec.Rules[0].Host)
+        fmt.Println(hosts)
 	}
 }
 
-func ManageServiceChanges(clientset *kubernetes.Clientset, searchDomain string, hosts *ConcurrentHostsFile) {
+func ManageServiceChanges(clientset *kubernetes.Clientset, searchDomain string, hosts *hostsfile.ConcurrentHostsFile) {
 	api := clientset.CoreV1()
 	listOptions := metav1.ListOptions{}
 	watcher, err := api.Services("").Watch(context.Background(), listOptions)
@@ -150,21 +122,12 @@ func ManageServiceChanges(clientset *kubernetes.Clientset, searchDomain string, 
 			continue
 		}
 
-		hosts.Lock.Lock()
 		// Serivces don't include the full search domain, so append it.
 		serviceName := service.ObjectMeta.Name
 		serviceIP := service.Spec.LoadBalancerIP
 
-		fqdn := serviceName + "." + searchDomain
-		hosts.Entries[fqdn] = HostsEntry{serviceIP, []string{fqdn}}
-		PrintHostsFile(hosts.Entries)
-		fmt.Println("")
-		hosts.Lock.Unlock()
-	}
-}
-
-func PrintHostsFile(hostsEntries map[string]HostsEntry) {
-	for _, he := range hostsEntries {
-		fmt.Println(he)
+        fqdn := serviceName + "." + searchDomain
+        hosts.AddHostname(serviceIP, fqdn)
+        fmt.Println(hosts)
 	}
 }
---
+++ pkg/hostsfile/hostsfile.go
@@ -0,0 +1,54 @@
+package hostsfile
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+)
+
+type HostsEntry struct {
+	ip    string
+	hosts []string
+}
+
+func (he HostsEntry) String() string {
+	return fmt.Sprintf(strings.Join(append([]string{he.ip}, he.hosts...), "\t"))
+}
+
+type ConcurrentHostsFile struct {
+	lock    *sync.RWMutex
+	entries map[string]*HostsEntry
+}
+
+func NewConcurrentHostsFile() *ConcurrentHostsFile {
+	mutex := sync.RWMutex{}
+	chf := ConcurrentHostsFile{&mutex, map[string]*HostsEntry{}}
+	return &chf
+}
+
+func (chfptr *ConcurrentHostsFile) AddHostname(ip string, hostname string) {
+	chf := *chfptr
+	chf.lock.Lock()
+    entry, present := chf.entries[ip]
+    if !present {
+		fmt.Println("Didn't find", ip, hostname)
+		he := HostsEntry{ip, []string{}}
+        entry = &he
+        chf.entries[ip] = entry
+    }
+
+    entry.hosts = append(entry.hosts, hostname)
+	chf.lock.Unlock()
+}
+
+func (chfptr *ConcurrentHostsFile) String() string {
+	chf := *chfptr
+	chf.lock.RLock()
+	var sb strings.Builder
+	for _, hostEntry := range chf.entries {
+		sb.WriteString(hostEntry.String())
+		sb.WriteString("\n")
+	} 
+	chf.lock.RUnlock()
+	return sb.String()
+}
