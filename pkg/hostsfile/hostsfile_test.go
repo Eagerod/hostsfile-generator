5a7ddea7dcb476ab67e3f16bf2bc41f2a157ffef
Aleem Haji <hajial@gmail.com>
2021-11-07 19:55:17 -0500

    fmt

--- pkg/hostsfile/hostsfile_test.go
+++ pkg/hostsfile/hostsfile_test.go
@@ -1,9 +1,6 @@
 package hostsfile
 
 import (
-// 	"fmt"
-// 	"strings"
-// 	"sync"
 	"testing"
 )
 
@@ -13,10 +10,10 @@ import (
 
 func TestHostsEntryString(t *testing.T) {
 	var tests = []struct {
-		name string
-		ip  string
-		hosts  []string
-		rv  string
+		name  string
+		ip    string
+		hosts []string
+		rv    string
 	}{
 		{"One Domain", "192.168.1.2", []string{"google.com"}, "192.168.1.2	google.com"},
 		{"Multiple Domains", "192.168.1.2", []string{"google.com", "www.google.com"}, "192.168.1.2	google.com	www.google.com"},
