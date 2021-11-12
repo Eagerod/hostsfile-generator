d1bf1c29248a5fa27739aebc4be1beb0e8a0084a
Aleem Haji <hajial@gmail.com>
2021-11-11 22:41:43 -0500

    Bake the updates channel into the HFD

--- pkg/daemon/hostsfile_daemon.go
+++ pkg/daemon/hostsfile_daemon.go
@@ -27,46 +27,145 @@ type DaemonResourceMonitor interface {
 }
 
 type HostsFileDaemon struct {
-	config    DaemonConfig
-	hostsfile hostsfile.IHostsFile
+	config         DaemonConfig
+	hostsfile      hostsfile.IHostsFile
+	updatesChannel chan bool
 }
 
 type IHostsFileDaemon interface {
 	Run()
 
-	Monitor(c chan<- bool, drm DaemonResourceMonitor)
+	Monitor(drm DaemonResourceMonitor)
 }
 
 func NewHostsFileDaemon(config DaemonConfig) *HostsFileDaemon {
-	hfd := HostsFileDaemon{config, hostsfile.NewConcurrentHostsFile()}
+	hfd := HostsFileDaemon{
+		config,
+		hostsfile.NewConcurrentHostsFile(),
+		make(chan bool, 100),
+	}
 	return &hfd
 }
 
 func (hfd *HostsFileDaemon) Run() {
-	updatesChannel := make(chan bool, 100)
-	defer close(updatesChannel)
+	defer close(hfd.updatesChannel)
 
-	go hfd.performUpdates(updatesChannel)
-	go hfd.Monitor(updatesChannel, &DaemonIngressMonitor{hfd.config.IngressIp, hfd.config.SearchDomain})
-	go hfd.Monitor(updatesChannel, &DaemonServiceMonitor{hfd.config.SearchDomain})
-	go updateAfterInterval(updatesChannel, time.Second*60)
+	go hfd.performUpdates()
+	go hfd.Monitor(&DaemonIngressMonitor{hfd.config.IngressIp, hfd.config.SearchDomain})
+	go hfd.Monitor(&DaemonServiceMonitor{hfd.config.SearchDomain})
+	go hfd.updateAfterInterval(time.Second*60)
 
 	interrupt.WaitForAnySignal(syscall.SIGINT, syscall.SIGTERM)
 }
 
+func (hfd *HostsFileDaemon) Monitor(drm DaemonResourceMonitor) {
+	// Resync every minute, just in case something somehow gets missed.
+	informerFactory := informers.NewSharedInformerFactory(hfd.config.KubernetesClientSet, time.Minute)
+
+	drm.Informer(informerFactory).AddEventHandler(
+		cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				objectId, err := drm.ValidateResource(obj)
+				if err != nil {
+					return
+				}
+
+				if hfd.hostsfile.SetHostsEntry(objectId, drm.GetResourceHostsEntry(obj)) {
+					log.Printf("Creating entry for %s: %s\n", drm.Name(), objectId)
+					hfd.updatesChannel <- true
+				}
+			},
+			DeleteFunc: func(obj interface{}) {
+				objectId, err := drm.ValidateResource(obj)
+				if err != nil {
+					return
+				}
+
+				if hfd.hostsfile.RemoveHostsEntry(objectId) {
+					log.Printf("Remove entry for %s: %s\n", drm.Name(), objectId)
+					hfd.updatesChannel <- true
+				}
+			},
+			UpdateFunc: func(oldObj, newObj interface{}) {
+				objectId, err := drm.ValidateResource(newObj)
+				if err != nil {
+					if objectId != "" &&  hfd.hostsfile.RemoveHostsEntry(objectId) {
+						log.Printf("Removing outdated entry %s: %s\n", drm.Name(), objectId)
+						hfd.updatesChannel <- true
+					}
+					return
+				}
+
+				if hfd.hostsfile.SetHostsEntry(objectId, drm.GetResourceHostsEntry(newObj)) {
+					log.Printf("Updating entry for %s: %s\n", drm.Name(), objectId)
+					hfd.updatesChannel <- true
+				}
+			},
+		},
+	)
+
+	stop := make(chan struct{})
+	informerFactory.Start(stop)
+	informerFactory.WaitForCacheSync(stop)
+}
+
+func (hfd *HostsFileDaemon) performUpdates() {
+	lastUpdate := time.Now()
+	for _ = range hfd.updatesChannel {
+		// Check the length of the channel before doing anything.
+		// If there are more items in it, just let the next iteration
+		//    handle the update.
+		if len(hfd.updatesChannel) >= 1 {
+			continue
+		}
+
+		hostsfile := hfd.hostsfile.String()
+		// If the last update was more than 60 seconds ago, write this one
+		//   immediately
+		if time.Now().Sub(lastUpdate).Minutes() >= 1 {
+			log.Println("Last update was more than 1 minute ago. Updating immediately.")
+			err := WriteHostsFileAndRestartPihole(&hfd.config, hostsfile)
+			if err != nil {
+				log.Fatal(err)
+			}
+			lastUpdate = time.Now()
+			continue
+		}
+
+		// Wait 3 seconds.
+		log.Println("Waiting 1 seconds before attempting hostsfile update.")
+		time.Sleep(time.Second * 1)
+		if len(hfd.updatesChannel) >= 1 {
+			log.Println("Aborting hostsfile update. Newer hostsfile is pending.")
+			continue
+		}
+
+		err := WriteHostsFileAndRestartPihole(&hfd.config, hostsfile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		lastUpdate = time.Now()
+	}
+}
+
+func (hfd *HostsFileDaemon) updateAfterInterval(delay time.Duration) {
+	time.Sleep(delay)
+	log.Println("Forcing update to ensure consistency")
+	hfd.updatesChannel <- true
+}
+
 func WriteHostsFileAndRestartPihole(daemonConfig *DaemonConfig, hostsfile string) error {
 	log.Println("Updating kube.list in pod:", daemonConfig.PiholePodName)
-	fmt.Println(hostsfile)
-	// if err := CopyFileToPod(daemonConfig, "/etc/pihole/kube.list", hostsfile); err != nil {
-	// 	return err
-	// }
+	if err := CopyFileToPod(daemonConfig, "/etc/pihole/kube.list", hostsfile); err != nil {
+		return err
+	}
 
-	// log.Println("Restarting DNS service in pod:", daemonConfig.PiholePodName)
-	// if err := ExecInPod(daemonConfig, []string{"pihole", "restartdns"}); err != nil {
-	// 	return err
-	// }
+	log.Println("Restarting DNS service in pod:", daemonConfig.PiholePodName)
+	if err := ExecInPod(daemonConfig, []string{"pihole", "restartdns"}); err != nil {
+		return err
+	}
 
-	// log.Println("Successfully restarted DNS service in pod:", daemonConfig.PiholePodName)
+	log.Println("Successfully restarted DNS service in pod:", daemonConfig.PiholePodName)
 	return nil
 }
 
@@ -106,100 +205,4 @@ func ExecInPod(daemonConfig *DaemonConfig, command []string) error {
 		Stdout: os.Stdout,
 		Stderr: os.Stderr,
 	})
-}
-
-func (hfd *HostsFileDaemon) Monitor(c chan<- bool, drm DaemonResourceMonitor) {
-	// Resync every minute, just in case something somehow gets missed.
-	informerFactory := informers.NewSharedInformerFactory(hfd.config.KubernetesClientSet, time.Minute)
-
-	drm.Informer(informerFactory).AddEventHandler(
-		cache.ResourceEventHandlerFuncs{
-			AddFunc: func(obj interface{}) {
-				objectId, err := drm.ValidateResource(obj)
-				if err != nil {
-					return
-				}
-
-				if hfd.hostsfile.SetHostsEntry(objectId, drm.GetResourceHostsEntry(obj)) {
-					log.Printf("Creating entry for %s: %s\n", drm.Name(), objectId)
-					c <- true
-				}
-			},
-			DeleteFunc: func(obj interface{}) {
-				objectId, err := drm.ValidateResource(obj)
-				if err != nil {
-					return
-				}
-
-				if hfd.hostsfile.RemoveHostsEntry(objectId) {
-					log.Printf("Remove entry for %s: %s\n", drm.Name(), objectId)
-					c <- true
-				}
-			},
-			UpdateFunc: func(oldObj, newObj interface{}) {
-				objectId, err := drm.ValidateResource(newObj)
-				if err != nil {
-					if objectId != "" &&  hfd.hostsfile.RemoveHostsEntry(objectId) {
-						log.Printf("Removing outdated entry %s: %s\n", drm.Name(), objectId)
-						c <- true
-					}
-					return
-				}
-
-				if hfd.hostsfile.SetHostsEntry(objectId, drm.GetResourceHostsEntry(newObj)) {
-					log.Printf("Updating entry for %s: %s\n", drm.Name(), objectId)
-					c <- true
-				}
-			},
-		},
-	)
-
-	stop := make(chan struct{})
-	informerFactory.Start(stop)
-	informerFactory.WaitForCacheSync(stop)
-}
-
-func (hfd *HostsFileDaemon) performUpdates(updatesChannel <-chan bool) {
-	lastUpdate := time.Now()
-	for _ = range updatesChannel {
-		// Check the length of the channel before doing anything.
-		// If there are more items in it, just let the next iteration
-		//    handle the update.
-		if len(updatesChannel) >= 1 {
-			continue
-		}
-
-		hostsfile := hfd.hostsfile.String()
-		// If the last update was more than 60 seconds ago, write this one
-		//   immediately
-		if time.Now().Sub(lastUpdate).Minutes() >= 1 {
-			log.Println("Last update was more than 1 minute ago. Updating immediately.")
-			err := WriteHostsFileAndRestartPihole(&hfd.config, hostsfile)
-			if err != nil {
-				log.Fatal(err)
-			}
-			lastUpdate = time.Now()
-			continue
-		}
-
-		// Wait 3 seconds.
-		log.Println("Waiting 1 seconds before attempting hostsfile update.")
-		time.Sleep(time.Second * 1)
-		if len(updatesChannel) >= 1 {
-			log.Println("Aborting hostsfile update. Newer hostsfile is pending.")
-			continue
-		}
-
-		err := WriteHostsFileAndRestartPihole(&hfd.config, hostsfile)
-		if err != nil {
-			log.Fatal(err)
-		}
-		lastUpdate = time.Now()
-	}
-}
-
-func updateAfterInterval(c chan<- bool, delay time.Duration) {
-	time.Sleep(delay)
-	log.Println("Forcing update to ensure consistency")
-	c <- true
 }
